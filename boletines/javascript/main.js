3b031e09ca95fbaa64c88f4823990d9eaf11c827
edgarluf04 <60110847+edgarluf04@users.noreply.github.com>
2020-01-21 19:50:55 -0600

    Update main.js

--- boletines/javascript/main.js
+++ boletines/javascript/main.js
@@ -18,7 +18,7 @@ links.forEach(function(link){
      content.classList.add("animated");
 
      setTimeout(function(){
-       location.href = "/";
+       location.href = "/boletines/";
      },600);
 
      return false;
