7b19db26c04e462955de08827ef960b0ec483f9e
edgarluf04 <60110847+edgarluf04@users.noreply.github.com>
2020-01-21 19:46:20 -0600

    Agregamos el proyecto boletines

---
+++ boletines/javascript/main.js
@@ -0,0 +1,27 @@
+
+let links = document.querySelectorAll(".close");
+
+//Recorrer
+
+links.forEach(function(link){
+   //agregar un evento click a cada mundo
+   link.addEventListener("click",function(ev){
+     ev.preventDefault();
+     let content = document.querySelector('.content');
+
+     //quitar clases de animaciones que ya tiene
+     content.classList.remove("fadeInDown");
+     content.classList.remove("animated");
+
+     //agregar clases para animar salida
+     content.classList.add("fadeOutUp");
+     content.classList.add("animated");
+
+     setTimeout(function(){
+       location.href = "/";
+     },600);
+
+     return false;
+
+   })
+})
